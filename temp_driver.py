e239a263cf86394820b8aee1453f52f5557122a3
troychibbaro <44310656+troychibbaro@users.noreply.github.com>
2021-09-29 13:04:40 -0400

    driver
    run application for emulated temperature logging. Adjust SenseHat module imports for hardware use.

---
+++ temp_driver.py
@@ -0,0 +1,53 @@
+from sense_emu import SenseHat
+from time import sleep
+import csv
+from datetime import datetime
+# You can change sense_emu to sense_hat for physical SenseHat hardware use
+sense = SenseHat()
+
+#method to return double temp as string
+def get_temp_string ():
+    temp = sense.get_temperature()
+    return str(temp)
+
+#Formats date to set as file name
+date = datetime.now()
+formatted_date = date.strftime("%m_%d_%Y")
+
+#Prompt for number of iterations and read
+print("Enter number of iterations: ")
+count = input()
+
+#open file to write temp data
+with open(formatted_date + '_readings' + '.csv', mode = 'w') as temp_file:
+    
+    #Create csvwriter object and write header
+    temp_writer = csv.writer(temp_file, delimiter = ',', quotechar = "'", quoting = csv.QUOTE_MINIMAL)
+    temp_writer.writerow(["Temperature", "Time"])
+    
+    #Iterate and read temperatures
+    for element in range(count):
+        
+        #retrieve temp
+        temp = get_temp_string()
+        
+        #print temp to console and set LED matrix
+        print(temp)
+        sense.show_message(temp + " degrees" , text_colour = [0, 255, 0], scroll_speed = 0.025)
+        
+        #take time and write line of data to .csv file
+        date = datetime.now()
+        temp_writer.writerow([temp, date.strftime("%H:%M:%S")])
+        
+        
+        #freeze loop for 2 seconds
+        sleep(2)
+        
+temp_file.close()
+
+
+
+
+
+
+
